040bf19669aa0548170835b8b3dacc4eb943217e
Goofygiraffe06 <rahul.narsing06@gmail.com>
2025-07-03 19:47:54 +0530

    chore: add basic chi server with /health and logging

---
+++ cmd/zinc/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	port := ":8000"
+	log.Printf("Listening on port %s", port)
+
+	err := http.ListenAndServe(port, router)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
