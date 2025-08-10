c37c9a97222d2d5b71bc9f907fb4262f4eb1d1d8
Goofygiraffe06 <rahul.narsing06@gmail.com>
2025-08-10 17:37:56 +0530

    refactor(auth): added logging support for key.go

--- internal/auth/key.go
+++ internal/auth/key.go
@@ -4,6 +4,9 @@ import (
 	"crypto/ed25519"
 	"crypto/rand"
 	"sync"
+	"time"
+
+	"github.com/Goofygiraffe06/zinc/internal/logging"
 )
 
 type SigningKey struct {
@@ -16,20 +19,30 @@ var (
 	once       sync.Once
 )
 
-// Call this during app startup (main.go)
 func InitSigningKey() {
 	once.Do(func() {
+		start := time.Now()
+		logging.DebugLog("Ed25519 key generation started")
+
 		pub, priv, err := ed25519.GenerateKey(rand.Reader)
 		if err != nil {
+			logging.ErrorLog("Ed25519 key generation failed: %v", err)
 			panic("failed to generate Ed25519 key: " + err.Error())
 		}
+
 		signingKey = &SigningKey{
 			PrivateKey: priv,
 			PublicKey:  pub,
 		}
+
+		duration := time.Since(start)
+		logging.InfoLog("Ed25519 key generation success %v", duration)
 	})
 }
 
 func GetSigningKey() *SigningKey {
+	if signingKey == nil {
+		logging.WarnLog("Signing key accessed before initialization")
+	}
 	return signingKey
 }
